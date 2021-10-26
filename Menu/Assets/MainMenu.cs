532d0886394d84cde7302249c84c4651133b1a4b
generasirabbani <generasirabbani@apps.ipb.ac.id>
2021-10-27 00:18:37 +0700

    Added stars

--- Menu/Assets/MainMenu.cs
+++ Menu/Assets/MainMenu.cs
@@ -12,6 +12,21 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void BackGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void OptionGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void BackOption()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
     public void QuitGame ()
     {
         Debug.Log("QUIT!");
